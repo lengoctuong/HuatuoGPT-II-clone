a99d501f35a7da4fe16db1e4676bca8f43766e8d
lengoctuong <lengoctuong23052002@gmail.com>
2025-06-07 20:12:47 +0700

    update en data

--- adaption/one_stage_training/train.sh
+++ adaption/one_stage_training/train.sh
@@ -24,23 +24,23 @@ experiment_name=HuatuoGPT2_7B
 #     --learning_rate 1e-4 \
 #     --gradient_checkpointing > training.log 2>&1 &
 
-accelerate launch --config_file ./adaption/one_stage_training/training_config/zero.yaml \
-    --num_processes 8 \
+!cd HuatuoGPT-II-clone; accelerate launch --config_file ./adaption/one_stage_training/training_config/zero.yaml \
+    --num_processes 1 \
     --num_machines 1 \
     --machine_rank 0 \
-    --deepspeed_multinode_launcher standard train_huatuo.py \
-    --experiment_name HuatuoGPT2_7B \
-    --model_path baichuan-inc/Baichuan2-13B-Base \
+    --deepspeed_multinode_launcher standard adaption/one_stage_training/train_huatuo.py \
+    --experiment_name Qwen3_0.6B \
+    --model_path Qwen/Qwen3-0.6B \
     --max_seq_len 4096 \
-    --gradient_accumulation_steps 4 \
-    --data_path ./sample_train_qa_0.01p_Baichuan2-13B-Base_4096_dataset/data-00000-of-00001.arrow \
+    --gradient_accumulation_steps 1 \
+    --data_path ./train_qa_0.01p_Baichuan2-13B-Base_4096_dataset \
     --output_dir ./ckpts \
     --log_dir ./train_logs \
     --n_epochs 2 \
     --warmup_rates 0.01 \
-    --train_bsz_per_gpu 4 \
-    --eval_bsz_per_gpu 4 \
+    --train_bsz_per_gpu 1 \
+    --eval_bsz_per_gpu 1 \
     --learning_rate 1e-4 \
-    --gradient_checkpointing > training.log 2>&1 &
+    --gradient_checkpointing > training.log 2>&1
 
 # sed -i 's/\r$//' adaption/one_stage_training/train.sh
